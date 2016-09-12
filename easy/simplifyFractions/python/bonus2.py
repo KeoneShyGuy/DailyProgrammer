b73ff7e48c14e831cda67603b0e5d141ab62bee7
KeoneShyGuy <keone.robinson@gmail.com>
2016-09-12 08:43:03 -0400

    How for the hard part

--- easy/simplifyFractions/python/bonus2.py
+++ easy/simplifyFractions/python/bonus2.py
@@ -9,5 +9,16 @@ Vars = Bonus[1: NumOfVars + 1]
 for idx, item in enumerate(Vars):
     Vars[idx] = item.rstrip("\n").split(" ")
 
+VarDict = {}
+for item in Vars:
+    TempDict = {item[0]: item[1]}
+    VarDict.update(TempDict)
+# There's probably a more efficient and less readable way to do this. Whatever
+# Now we replace the remaining lines with the variables if they have any
+# let's make a list out of the fractions we're supposed to simplify
+FractionList = []
+for num in range(NumOfVars + 1, len(Bonus)):
+    FractionList.append(Bonus[num].rstrip("\n").split(" "))
+# sexy. Now we can replace the Fractions with their Equations
 
-print Vars
+print FractionList
