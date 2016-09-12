ee0a42b8456629857948a1cada505c604d6118d8
KeoneShyGuy <keone.robinson@gmail.com>
2016-09-12 07:13:57 -0400

    Got the variables.

--- easy/simplifyFractions/python/bonus2.py
+++ easy/simplifyFractions/python/bonus2.py
@@ -1,6 +1,13 @@
-bonus = open("bonusSource.txt", "r").readlines()
+# https://redd.it/4uhqdb
+Bonus = open("bonusSource.txt", "r").readlines()
 
 # first, we need to figure out how many variable there will be.
-numOfVars = bonus[0]
+NumOfVars = int(Bonus[0])
 
-print numOfVars
+# next, we should grab those variables and make a dictionary out of them
+Vars = Bonus[1: NumOfVars + 1]
+for idx, item in enumerate(Vars):
+    Vars[idx] = item.rstrip("\n").split(" ")
+
+
+print Vars
