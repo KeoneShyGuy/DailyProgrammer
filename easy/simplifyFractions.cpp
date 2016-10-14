5680b9302d4a6f1aada6f8feb4987257887bddc2
KeoneShyGuy <KeoneShyGuy@users.noreply.github.com>
2016-10-14 05:35:59 -0400

    Add files via upload

---
+++ easy/simplifyFractions.cpp
@@ -0,0 +1,45 @@
+//https://redd.it/4uhqdb
+
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int gcd(int a, int b);
+
+int main(){
+    int x = 4;
+    int y = 8;
+    int answer[2];
+    string line;
+    ifstream myFile ("fractions.txt");
+
+    if (myFile.is_open()){
+        while (getline(myFile, line)){
+            cout << line << "\n";
+        }
+        myFile.close();
+    }
+    else cout << "Unable to open file\n";
+    int divisor = gcd(x, y);
+
+    answer[0] = x / divisor;
+    answer[1] = y / divisor;
+
+    printf( "%d %d", answer[0], answer[1]);
+
+
+    return 0;
+}
+
+int gcd(int a, int b){
+    int c;
+    while (b){
+        c = a;
+        a = b;
+        b = c%b;
+    }
+    return a;
+}
