ef8440a397315d7768d451788e1afcd7472a8ec8
KeoneShyGuy <KeoneShyGuy@users.noreply.github.com>
2016-10-19 17:14:47 -0400

    Add files via upload

--- medium/mathagram/mathagram.py
+++ medium/mathagram/mathagram.py
@@ -1,3 +1,6 @@
+# https://redd.it/576o8o
+from random import randint
+
 class mathagram(object):
 	
 	def __init__(self, first, second, third, 
@@ -5,16 +8,22 @@ class mathagram(object):
 						 seventh=None, eighth=None, ninth=None):
 		self.level = 0				 
 		self.unused_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		self.input = [first, second, third, fourth, fifth, sixth,
-							seventh, eighth, ninth]
-		for i,var_ in enumerate(self.input):
-			var_[i] = str(var_)
-		for option in self.input:
-			for char in option:
-				try:
-					self.unused_nums.remove(char)
-				except:
-					StandardError
+		self.input = [first, second, third, fourth, fifth, 				sixth, seventh, eighth, ninth]
+		"""
+		Need to find a way to cancel the function if the input
+		doesn't have 3,6, or 9 values
+		"""
+							
+		for opt in self.input:
+			if not opt is None:
+				for char_ in opt:
+					try:
+						if int(char_) in self.unused_nums:
+							self.unused_nums.remove(int(char_))
+					except ValueError:
+						pass
+
+		print self.unused_nums
 		self.first = first
 		self.second = second
 		self.third = third
@@ -28,11 +37,27 @@ class mathagram(object):
 			self.level = 1
 			print "Single"
 			
-		@property
-		def level(self):
-			return self.level
+	def guess(self):
+		a = list(self.first)
+		b = list(self.second)
+		c = list(self.third)
+		abc = [a, b, c]			
+		temp = self.unused_nums
+		c = 0
+		solved = False
+		while solved == False:
+			for i,x in enumerate(abc):
+				for i, int_ in enumerate(x):
+					if int_ == 'x':
+						g = randint(0, (len(temp)-1))
+						replace = temp[g]
+						x[i] = str(replace)
+						temp.remove(replace)
+				attempt = int(''.join(x))
+				print attempt
 			
 		
 		
-m = mathagram(7, 6, 9, 1, 3)
-print m.level
+m = mathagram('1xx', 'xxx', '468')
+
+m.guess()
