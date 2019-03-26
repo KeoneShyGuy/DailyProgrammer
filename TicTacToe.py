6bdd7c25094eec641bb9f66a2e4e4759a97e6a18
KeoneShyGuy <KeoneShyGuy@users.noreply.github.com>
2019-03-26 15:21:33 -0400

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -17,11 +17,34 @@ def createGrid(gridSize, defaultFill='_'):
 
 def placeMark(position, mark, arr):
     xKey = {'A': 0, 'B':1, 'C': 2}
-    x =  xKey[position[0].capitalize()]
-    y = int(position[1]) - 1
-    arr[x][y] = mark
+    xCheck, yCheck, lenCheck = False, False, False
+    print(len(position))
     
-def checkGrid(arr, bKeepGoing):
+    if (len(position) == 2):
+        lenCheck = True
+        print(lenCheck)
+    
+    if ((position[0].capitalize() in xKey) and lenCheck):
+        x = xKey[position[0].capitalize()]
+        xCheck = True        
+    
+    if (lenCheck):
+        y = int(position[1])-1
+        if (y >= 0 and y <= 2):
+            yCheck = True
+        
+    if (xCheck and yCheck and lenCheck):
+        if (arr[x][y] == '_'):
+            arr[x][y] = mark
+            return False
+        else:
+            print("There's already something there.\n")
+            return True
+    else:
+        print('Coordinates not recognized. Please try again.\n')
+        return True
+    
+def checkGrid(arr, bKeepGoing, mark):
     lineOne = (arr[0][0], arr[0][1], arr[0][2])
     lineTwo = (arr[1][0], arr[1][1], arr[1][2])
     lineThree = (arr[2][0], arr[2][1], arr[2][2])
@@ -35,25 +58,33 @@ def checkGrid(arr, bKeepGoing):
     
     for line in allLines:
         if ((line[0] == line[1] == line[2]) and line[0] != '_'):
-            print('WINNNER!')
-            bKeepGoing = 0
+            print(mark + ' is the WINNNER!')
+            bKeepGoing = False
+    return bKeepGoing
 
 testGrid = createGrid(3)
 turns = 0
-bStillPlay = 1
+bStillPlay = True
 
-while bStillPlay:    
-    playerChoice = input("Type a coordinate and press enter.\n" +
-                         "Enter the let 'H' to show coordinates.\n")
-    if (playerChoice.capitalize() == 'H'):
-        printGrid(helpGrid)
-
-    printGrid(testGrid)
-    print(turns)
+while bStillPlay:
+    bSpotTaken = True    
+    
     if ((turns % 2) == 0):
-        placeMark(playerChoice, 'X', testGrid)
+        playerMark = 'X'
     else:
-        placeMark(playerChoice, 'O', testGrid)
+        playerMark = 'O'    
+
+    while bSpotTaken:
+        playerChoice = input("Type a coordinate and press enter.\n" +
+                             "Enter the let 'H' to show coordinates.\n")
+        #printGrid(testGrid)
+        if (playerChoice.capitalize() == 'H'):
+            printGrid(helpGrid)
+            turns -= 1
+            break
+        bSpotTaken = placeMark(playerChoice, playerMark, testGrid)
+    # turn is still being updated and grid is still being
+    # printed when the player enters an error
     printGrid(testGrid)
-    checkGrid(testGrid, bStillPlay)
+    bStillPlay = checkGrid(testGrid, bStillPlay, playerMark)
     turns += 1
