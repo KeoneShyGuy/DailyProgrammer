367a92704732c7dd25e93962b914b03710f6a8b1
KeoneShyGuy <KeoneShyGuy@users.noreply.github.com>
2019-05-02 15:24:43 -0400

    Update AxisAlignedCrate.cs
    Progress, but still some problems.

--- AxisAlignedCrate.cs
+++ AxisAlignedCrate.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//https://www.reddit.com/r/dailyprogrammer/comments/bazy5j/20190408_challenge_377_easy_axisaligned_crate/
+
+// https://www.reddit.com/r/dailyprogrammer/comments/bazy5j/20190408_challenge_377_easy_axisaligned_crate/
 namespace DailyProgrammer
 {
     static class PySharp
@@ -12,7 +9,6 @@ namespace DailyProgrammer
         {
             Console.WriteLine("{0}:{1}", msg, value);
         }
-
         static public int Factorial(int num)
         {
             int i = 2;
@@ -24,7 +20,6 @@ namespace DailyProgrammer
             }
             return factor;
         }
-
         static public void PrintElement<T>(T[] arr)
         {
             if (arr == null)
@@ -34,14 +29,11 @@ namespace DailyProgrammer
                     Console.Write(elem + " ");            
         }
         // function mainly meant for permutations code
-        private static int[] Swap(int[] arr, int pos1, int pos2)
+        public static void Swap<T>(T[] intArr, int pos1, int pos2)
         {
-            int temp;
-            int[] intArr = arr;
-            temp = intArr[pos1];
+            T temp = intArr[pos1];
             intArr[pos1] = intArr[pos2];
             intArr[pos2] = temp;
-            return intArr;
         }
         // function for creating creating empyty jagged array to hold perms
         public static int[][] MakeJaggedForPerms(int arrLength, int permLength, bool bPrintRowNum=false)
@@ -53,21 +45,28 @@ namespace DailyProgrammer
             return new int[rows][];
         }
         // second attempt at permutations. A more simple one, not the one from Python.
-        static public void Permuations(int[] toPerm, int[][] storage, int startIdx=0, int endIdx=0)
+        static public void Permuations<T>(T[] toPerm, int startIdx, int endIdx, ref T[][]storageArr, ref int count)
         {
-            // https://www.geeksforgeeks.org/c-program-to-print-all-permutations-of-a-given-string-2/
-            int i = startIdx;
+            // https://www.geeksforgeeks.org/c-program-to-print-all-permutations-of-a-given-string-2/            
             if (startIdx == endIdx)
             {
-                storage[startIdx] = toPerm;                
+                for (int j = 0; j < toPerm.Length; j++)
+                {
+                    storageArr[count][j] = toPerm[j];
+                }
+                System.Console.Write("Current Perm: ");
+                PrintElement(toPerm);
+                System.Console.WriteLine("Count: " + count + "\n");
+                count++;
             }
             else
             {
-                for (i = startIdx; i <= endIdx; i++)
+                for (int i = startIdx; i <= endIdx; i++)
                 {
-                    toPerm = Swap(toPerm, startIdx, i);
-                    Permuations(toPerm, storage, startIdx + 1, endIdx);
-                    toPerm = Swap(toPerm, startIdx, i);
+                    Swap(toPerm, startIdx, i);
+                    Permuations(toPerm, startIdx + 1, endIdx, ref storageArr, ref count);
+                    Swap(toPerm, startIdx, i);
+                    
                 }
             }
         }        
@@ -76,33 +75,21 @@ namespace DailyProgrammer
     {
         static void Main(string[] args)
         {
-            // PySharp py = new PySharp();
-            /*
-            py.Display<int>("Int", 467);
-            py.Display<char>("Char", 'A');
-            py.Display<double>("Double", 45.67);
-            
-            string ham = "HAM";
-            char[] hamArr = ham.ToCharArray();
-            py.PrintElement<char>(hamArr);
-            
-            foreach (int num in PySharp.Range(-20, 3, -5))
-            {
-                Console.Write(num.ToString() + " ");
-            }
-            
-            */
-            // List<int> permTest = new List<int>(PySharp.Permutations(new int[] { 7, 8, 2, -2, 90 }, 2));
-            int[] testArr = new int[] { 5, 4, 7, 5, 12 };
+            int[] testArr = new int[] { 5, 4, 7 };
             int start = 0;
             int end = 2;
-            int lenOfPerm = end - start;
+            int lenOfPerm = testArr.Length - start;
             int[][] permPool = PySharp.MakeJaggedForPerms(testArr.Length, lenOfPerm, true);
-            PySharp.Permuations(testArr, permPool, start, end - 1);
-            for (int i = 0; i < permPool.Length; i++)
+            int insertArr = 0;
+
+            
+            PySharp.Permuations<int>(testArr, start, lenOfPerm - 1, ref permPool, ref insertArr);
+
+            foreach (int[] currentArr in permPool)
             {
-                PySharp.PrintElement(permPool[i]);
-                System.Console.WriteLine();
+                System.Console.Write("(");
+                PySharp.PrintElement(currentArr);
+                System.Console.WriteLine(")\n");
             }
 
             int Fit(int bigX, int bigY, int smallX, int smallY)
@@ -111,7 +98,6 @@ namespace DailyProgrammer
                 int totalY = bigY / smallY;
                 return totalX * totalY;
             }
-
             int Fit2(int bigX, int bigY, int smallX, int smallY)
             {
                 int fit1 = Fit(bigX, bigY, smallX, smallY);
@@ -119,7 +105,6 @@ namespace DailyProgrammer
                 int totalY = bigY / smallX;
                 return fit1 >= (totalX * totalY) ? fit1 : (totalX * totalY);
             }
-
             int Fit3(int bigX, int bigY, int bigZ,
                 int smallX, int smallY, int smallZ)
             {
@@ -129,8 +114,7 @@ namespace DailyProgrammer
 
                 return 0;
             }
-            int X = 10, Y = 10, Z = 10, x = 1, y = 1, z = 1;
-
+            
             // System.Console.WriteLine(Fit(X, Y, x, y));
 
             // System.Console.WriteLine(Fit2(X, Y, x, y));
